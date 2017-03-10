f352c3c4d50c912acae8543d7df4541898aca4f1
Jiri Spacek <jspacek@gemsystem.cz>
2017-03-10 12:07:52 +0100

    Include remote JS scripts locally and fix misc bugs

---
+++ credentials.js
@@ -0,0 +1,4 @@
+var openid        = "/openid";
+var client_id     = "sampleclient-openid-4e841c1c-0b4b-4ccc-a47e-fd2a7edb20bf";
+var client_secret = "b34666a8853242eeafdbb679f1a1cd83";
+var client_realm  = "/fo";
